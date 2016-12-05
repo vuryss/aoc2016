301d1efd226501ce76fdc8624d548716ad22779e
Georgi Georgiev <vuryss@gmail.com>
2016-12-05 07:26:07 +0200

    Day 5 - edit

--- day-5.1.php
+++ day-5.1.php
@@ -4,13 +4,9 @@ $input    = '--input--';
 $i        = 0;
 $password = '';
 
-while (true) {
+while (strlen($password) != 8) {
     $hash = md5($input . $i++);
-
-    if (strpos($hash, '00000') === 0) {
-        $password .= $hash[5];
-        if (strlen($password) == 8) break;
-    }
+    if (strpos($hash, '00000') === 0) $password .= $hash[5];
 }
 
 echo $password;
--- day-5.2.php
+++ day-5.2.php
@@ -4,13 +4,12 @@ $input    = '--input--';
 $i        = 0;
 $password = [];
 
-while (true) {
+while (count($password) != 8) {
     $hash = md5($input . $i++);
 
     if (strpos($hash, '00000') === 0) {
-        if (!ctype_digit($hash[5]) || (int) $hash[5] > 7 || isset($password[(int) $hash[5]])) continue;
-        $password[(int) $hash[5]] = $hash[6];
-        if (count($password) == 8) break;
+        if (!ctype_digit($hash[5]) || $hash[5] > 7 || isset($password[$hash[5]])) continue;
+        $password[$hash[5]] = $hash[6];
     }
 }
 
