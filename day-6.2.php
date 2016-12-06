34a6e13b760d3a599240cfd0265335f9f6991e40
Georgi Georgiev <ggeorgiev@shutterfly.com>
2016-12-06 17:56:44 +0200

    Day 6

---
+++ day-6.2.php
@@ -0,0 +1,17 @@
+<?php
+
+$input = '--input--';
+
+$inputRows  = explode("\n", $input);
+$mLength    = strlen($inputRows[0]);
+$characters = [];
+
+foreach ($inputRows as $row)
+    foreach (str_split($row) as $index => $character)
+        $characters[$index][$character] = ($characters[$index][$character] ?? 0) + 1;
+
+array_walk($characters, 'asort');
+$characters = array_map('array_flip', $characters);
+$characters = array_map('reset', $characters);
+
+echo implode('', $characters);
