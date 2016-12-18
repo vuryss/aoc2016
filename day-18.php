d3e6542d32bdc80e525910c1eeb7bb8cbe04ce16
Georgi Georgiev <vuryss@gmail.com>
2016-12-18 14:49:37 +0200

    Day 18 even shorter

--- day-18.php
+++ day-18.php
@@ -9,8 +9,7 @@ $nRows    = 400000; // 40 for part 1
 
 for ($i = 1, $row = []; $i < $nRows; $i++, $last = $row, $row = [])
     for ($j = 0; $j < $nColumns; $j++) {
-        list ($left, $center, $right) = [$last[$j - 1] ?? 0, $last[$j], $last[$j + 1] ?? 0];
-        $row[$j] = ($left && !$right || !$left && $right) ? 1 : 0;
+        $row[$j] = ($last[$j - 1] ?? 0) != ($last[$j + 1] ?? 0) ? 1 : 0;
         if (!$row[$j]) $safe++;
     }
 
