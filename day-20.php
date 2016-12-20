f7f68e628a31b1654cb2201dbd3b88adea4c6c9d
Georgi Georgiev <vuryss@gmail.com>
2016-12-20 11:05:38 +0200

    Day 20 optimized

--- day-20.php
+++ day-20.php
@@ -3,6 +3,7 @@
 $input = '--input--';
 
 $begin = $end = [];
+$banned = 0;
 
 foreach (explode("\n", $input) as $range) list($begin[], $end[]) = explode('-', $range);
 
@@ -11,25 +12,11 @@ array_multisort($begin, SORT_ASC, $end);
 for ($i = 0, $j = 1, $len = count($begin) - 1; $i < $len; $i++, $j = 1) {
     while (isset($begin[$i + $j]) && $end[$i] >= $begin[$i + $j] - 1) {
         if ($end[$i + $j] > $end[$i]) $end[$i] = $end[$i + $j];
-        unset($begin[$i + $j], $end[$i + $j]);
-        $j++;
+        unset($begin[$i + $j], $end[$i + $j++]);
     }
+    $banned += $end[$i] - $begin[$i] + 1;
     $i += $j - 1;
 }
 
-$begin = array_values($begin);
-$end   = array_values($end);
-
 echo 'First IP: ' . ($end[0] + 1) . PHP_EOL;
-
-$count = 0;
-
-for ($i = 0; $i <= 4294967295; $i++) {
-    if (false !== ($key = array_search($i, $begin))) {
-        $i = $end[$key];
-        continue;
-    }
-    $count++;
-}
-
-echo 'Allowed IPs: ' . $count . PHP_EOL;
+echo 'Allowed IPs: ' . (4294967295 - $banned + 1) . PHP_EOL;
